1f28baaf868b769ecb909e3d4c7a780d22db49de
Abdu <jentel@hotmail.com>
2022-01-28 11:11:42 -0800

    Add project files.

---
+++ HotmailJunkMailDeleter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+
+using Microsoft.Extensions.Configuration;
+
+namespace HotmailJunkMailDeleter
+{
+    class Program
+    {
+        private static IConfiguration configuration;
+
+
+        public Program()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            configuration = builder.Build();
+
+            
+            
+            string userName = configuration["Hotmail:UserName"];
+            string password = configuration["Hotmail:Password"];
+
+            Hotmail hotmail = new Hotmail(userName, password);
+            hotmail.Start();
+
+          
+        }
+
+        
+
+        
+
+        
+    }
+}
